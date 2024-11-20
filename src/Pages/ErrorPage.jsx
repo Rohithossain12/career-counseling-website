591ae1c757e875c5d3e50c28581d575fcd37cd23
Rohithossain12 <https://github.com/Rohithossain12>
2024-11-21 01:46:13 +0600

    update some info

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
-    return (
-        <div className='min-h-screen flex flex-col justify-center items-center space-y-3'>
-          <h1 className=' text-2xl md:text-4xl lg:text-7xl font-bold'>404</h1>  
-            <p className='md:text-xl font-bold'>Page Not Found</p>
-            <Link className='btn btn-primary font-bold' to='/'>Go Back Home</Link>
-        </div>
-    );
+  return (
+    <div className="min-h-screen  flex flex-col justify-center items-center space-y-3">
+      <h1 className=" text-2xl md:text-4xl lg:text-7xl font-bold">404</h1>
+      <p className="md:text-xl font-bold">Page Not Found</p>
+      <Link
+        className="bg-[#6D8CA0] text-white px-5 py-3 rounded-full font-bold"
+        to="/"
+      >
+        Go Back Home
+      </Link>
+    </div>
+  );
 };
 
 export default ErrorPage;
